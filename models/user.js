cc113ff7623ca9e6d73b43d77957bebe16dc065b
Kar Sheng Lee <karshenglee@Kars-MacBook-Air.local>
2017-04-22 23:33:21 +0800

    set up server auth

--- models/user.js
+++ models/user.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
 // Define model
-const userScheme = new Schema({
+const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
 	password: String
 });
 
 // On Save Hook, encrypt password
 // Before saving a model, run this function
-userScheme.pre('save', function(next) {
+userSchema.pre('save', function(next) {
 	// get access to the user model
 	const user = this;
 
@@ -29,8 +29,15 @@ userScheme.pre('save', function(next) {
 	});
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if (err) { return callback(err); }
+		callback(null, isMatch);
+	});
+};
+
 // Create the model class
-const ModelClass = mongoose.model('user', userScheme);
+const ModelClass = mongoose.model('user', userSchema);
 
 // Export the model
 module.exports = ModelClass;
